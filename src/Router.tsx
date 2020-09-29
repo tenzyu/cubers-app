3f4b1e7407facef254940c271451aba610c924c1
fe-amazu <tenzyu.on@gmail.com>
2020-09-30 04:08:39 +0900

    fix jump to 404 when we reload with HashRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  // BrowserRouter,
+  Route,
+  Switch,
+  HashRouter,
+} from "react-router-dom";
 import Index from "pages/index";
 import TutorialThree from "pages/3x3";
 import TutorialTwo from "pages/2x2";
@@ -8,14 +13,14 @@ import About from "pages/about";
 export default class Router extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={Index} />
           <Route path="/3x3" component={TutorialThree} />
           <Route path="/2x2" component={TutorialTwo} />
           <Route path="/about" component={About} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
