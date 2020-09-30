e5b32b65acbc2bcbda3be175e65c8310293eb850
fe-amazu <tenzyu.on@gmail.com>
2020-10-01 02:17:09 +0900

    rewrite 2x2.tsx

--- src/pages/2x2.tsx
+++ src/pages/2x2.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { IndexLayout } from "layouts/layout";
-import styled from "styled-components";
 
-const Title = styled.p`
-  text-align: center;
-  font-size: var(--h1-font-size);
-`;
+import styles from "styles/article.module.css";
 
 class TutorialTwo extends React.Component {
   render() {
     return (
       <IndexLayout>
-        <Title>2x2の解き方</Title>
+        <p className={styles.Title}>2x2の解き方</p>
       </IndexLayout>
     );
   }
