c1059d44e37637e51269ef01c26f6b212b2ae27b
fe-amazu <tenzyu.on@gmail.com>
2020-10-02 04:32:59 +0900

    removed unnecessary code in the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,20 +5,15 @@ import { Link } from "react-router-dom";
 
 export const Header = () => (
   <header className={styles.header} role="banner">
-    <div className={styles.left}>
-      <h1 className={styles.logo}>
-        <Link to="/">cubers.app</Link>
-      </h1>
-    </div>
-
-    <nav className={styles.right}>
-      <ul className={styles.ul}>
-        {["3x3", "2x2", "About"].map((name) => (
-          <li className={styles.li}>
-            <Link to={`/${name.toLowerCase()}`}> {name} </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <h1>
+      <Link to="/">cubers.app</Link>
+    </h1>
+    <ul className={styles.navlinks}>
+      {["3x3", "2x2", "About"].map((name) => (
+        <li>
+          <Link to={`/${name.toLowerCase()}`}> {name} </Link>
+        </li>
+      ))}
+    </ul>
   </header>
 );
