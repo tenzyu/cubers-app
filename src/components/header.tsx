3a32e6811718e570b6594b093be184653d06a0ad
fe-amazu <tenzyu.on@gmail.com>
2020-10-02 04:34:48 +0900

    .header -> header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import styles from "styles/header.module.css";
 import { Link } from "react-router-dom";
 
 export const Header = () => (
-  <header className={styles.header} role="banner">
+  <header role="banner">
     <h1>
       <Link to="/">cubers.app</Link>
     </h1>
