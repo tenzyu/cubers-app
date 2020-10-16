6882c31a84b81c84c5b0c9c5a03cdd0d30332061
fe-amazu <tenzyu.on@gmail.com>
2020-10-16 23:52:18 +0900

    use mdx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { SingleLayout } from "layouts/singleLayout";
 
+import { importMDX } from 'mdx.macro';
 import { Article } from "components/article";
+
+const Content = React.lazy(() => importMDX('../contents/about/about.mdx'))
 
 class About extends React.Component {
   render() {
     return (
       <SingleLayout>
         <Article title="Cubers App" description="about page">
-          <p>Todo: マークダウンで入稿できるようにする</p>
         </Article>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Content />
+        </React.Suspense>
       </SingleLayout>
     );
   }
